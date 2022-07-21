058d76f5510b22a5712a78b20d550a6ce910b344
Irene Njeru <irenenjeru8@gmail.com>
2022-07-21 13:13:20 +0300

    task completed

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,23 @@
+class PlantsController < ApplicationController
+    wrap_parameters format: []
+    def index
+      plants = Plant.all
+      render json: plants
+    end
+  
+    def show
+      plant = Plant.find(params[:id])
+      render json: plant
+    end
+  
+    def create
+      newPlant = Plant.create(plant_params)
+      render json: newPlant, status: :created
+    end
+  
+    private
+  
+    def plant_params
+      params.permit(:name, :image, :price)
+    end
+  end
